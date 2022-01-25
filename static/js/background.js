b3e4205548fe2b35798d52f86d302b7c1c84d44d
jota-ele-ene <hi@jln.bz>
2022-01-25 01:32:46 +0100

    Adding Unsplash

--- static/js/background.js
+++ static/js/background.js
@@ -1,11 +1,3 @@
-var orientation = getOrientation();
-var b = document.getElementsByTagName("BODY")[0];
-b.setAttribute("orientation", orientation);
-window.addEventListener("resize", checkOrientation);
-
-processOrientation();
-
-next_bg();
 
 function next_bg() {
   var a = document.querySelector(".container_bg.bg_active");
@@ -20,6 +12,8 @@ function next_bg() {
   var x = document.querySelectorAll(".container_bg.fade_out")
   if (x.length>0) {
     var i = getRandomArbitrary(0,x.length-1);
+    var image = x[i].querySelector('.image');
+    image.style.backgroundImage = "url('"+image.getAttribute("refer");+"')";
     x[i].classList.toggle("fade_out");
     x[i].classList.toggle("fading");
     x[i].classList.toggle("bg_active");
@@ -66,6 +60,7 @@ function checkOrientation() {
         a.classList.toggle("fade_out");
         a.classList.toggle("fading");
       }
+      getRandomImages();
       processOrientation();
       next_bg();
     }
@@ -75,6 +70,7 @@ function processOrientation() {
   var b = document.getElementsByTagName("BODY")[0];
   var orientation = b.getAttribute("orientation");
   var x = document.querySelectorAll(".container_bg");
+  console.log("Backgrounds available: "+(x.length));
   let not_fitting = 0;
   for (let i = 0; i < x.length; i++) {
     let value = x[i].getAttribute("size");
@@ -83,6 +79,7 @@ function processOrientation() {
     if (aSize[0]>aSize[1]) text = "landscape";
     else if (aSize[0]<aSize[1]) text = "portrait";
     else text = "square";
+    console.log(text + " vs. " + orientation);
     if (text.localeCompare(orientation))
     {
       x[i].classList.toggle("not_fit");
@@ -92,3 +89,42 @@ function processOrientation() {
   }
   console.log("Backgrounds available for "+ orientation +": "+(x.length-not_fitting))
 }
+
+
+function getRandomImages(url) {
+
+  console.log("Getting images from Unsplash: "+ url)
+
+  fetch(url)
+    .then((res) => res.json())
+    .then((res) => {
+      console.log(res);
+      res.forEach(addImageContainer);
+      processOrientation();
+      next_bg();
+    })
+    .catch(function (error) {
+      console.log("dataLoadingError:"+error);
+    });
+
+
+}
+
+
+  function addImageContainer(item) {
+    var container_html = '<div class="container_bg fade_out" size="'+ item.width + 'x' + item.height +'">';
+    container_html += '<div class="image image_fadedOut" refer="' + item.urls.regular + '" rel="nofollow" download=""></div>';
+    container_html += '<div id="credits-box">';
+    container_html +=   '<div class="out info" onclick="toggle_info()"><i class="fas fa-info-circle"></i></div>';
+    container_html +=   '<div class="credits info fade_out" onclick="toggle_info()">';
+    container_html +=     '<div class="font-credits">';
+    container_html +=       '<span>Background credits: </span>';
+    container_html +=       '<span>by <a href="https://unsplash.com/@' + item.user.username + '" target="_blank">' + item.user.name + '</a> </span>';
+    container_html +=       '<span>available <a href="' + item.links.download_location + '" target="_blank">here</a></span>';
+    container_html +=     '</div>';
+    container_html +=   '</div>';
+    container_html += '</div>';
+    container_html += '</div>';
+    var bg = document.getElementsByClassName('bg');
+    bg[0].insertAdjacentHTML('beforeend', container_html);
+  }
